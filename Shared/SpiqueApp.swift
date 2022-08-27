39b68844d7237527dfab3cda8b0bcbe1cafbac85
Austin Condiff <austin.condiff@gmail.com>
2022-08-27 15:50:30 -0500

    Initial Commit

---
+++ Shared/SpiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiqueApp.swift
+//  Shared
+//
+//  Created by Austin Condiff on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct SpiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
